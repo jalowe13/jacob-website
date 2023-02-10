e5f00f5ff02292156e4debad82d315a4b9f6e4f6
jalowe13 <jplowe21@vt.edu>
2023-02-10 14:24:09 -0500

    Heroku for SSL

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 // HTTPS in NextJS
 // https://nextjs.org/docs/advanced-features/custom-server
+// Middleware redirect
+// https://stackoverflow.com/questions/66458059/how-do-i-force-or-redirect-my-next-js-website-to-use-https
 
 const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
+const { default: sslRedirect } = require('heroku-ssl-redirect')
+
+//Middleware
+const express = require('express')
 
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
@@ -13,27 +19,16 @@ const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  createServer(async (req, res) => {
-    try {
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
-      const parsedUrl = parse(req.url, true)
-      const { pathname, query } = parsedUrl
+    const server = express();
 
-      if (pathname === '/a') {
-        await app.render(req, res, '/a', query)
-      } else if (pathname === '/b') {
-        await app.render(req, res, '/b', query)
-      } else {
-        await handle(req, res, parsedUrl)
-      }
-    } catch (err) {
-      console.error('Error occurred handling', req.url, err)
-      res.statusCode = 500
-      res.end('internal server error')
-    }
-  }).listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://${hostname}:${port}`)
-  })
+    //Redirect
+    server.use(sslRedirect());
+    server.all('*', (req, res) => {
+        return handle(req, res);
+    });
+
+    server.listen(port, (err) => {
+        if (err) throw err
+        console.log(`> Ready on http://${hostname}:${port}`)
+    })
 })
