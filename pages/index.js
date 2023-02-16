1f969b21a3eecf301b6e47a0c4aa14bfca794699
jalowe13 <jplowe21@vt.edu>
2023-02-15 20:56:26 -0500

    typo

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
           "A Recent Graduate",
           "Honest",
           "Hardworking",
-          "Consistantly Improving",
+          "Consistently Improving",
           "A Learner",
           "Curious",
           "Joyful",
